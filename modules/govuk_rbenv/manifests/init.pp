c9f46ca4c9eabaa8cbf79b38ad9726135be0ef9e
Dean Wilson <dean.wilson@digital.cabinet-office.gov.uk>
2016-09-23 15:29:43 +0100

    Install ruby 2.1.8 everywhere as we're going to use it for puppet
    We're leaving 1.9.3 in for now while we upgrade the puppet used ruby.
    We're also unsure if we have any other scripts running that use this version.

--- modules/govuk_rbenv/manifests/init.pp
+++ modules/govuk_rbenv/manifests/init.pp
@@ -6,6 +6,15 @@
 class govuk_rbenv {
   include rbenv
 
+  rbenv::version { '2.1.8':
+    bundler_version => '1.10.6',
+  }
+  rbenv::alias { '2.1':
+    to_version => '2.1.8',
+  }
+
+  # FIXME: we're leaving this in for now while we upgrade the puppet used ruby. 
+  # We're also unsure if we have any other scripts running.
   rbenv::version { '1.9.3-p550':
     bundler_version => '1.7.4',
   }
